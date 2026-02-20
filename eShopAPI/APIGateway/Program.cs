c3250198038c2c711a3d8f68f6e1f3148cfb80f7
Kapil Pandey <Kapil Pandey@KAPIL-PC>
2026-02-20 11:52:43 +0530

    put authentication JWT token

---
+++ eShopAPI/APIGateway/Program.cs
@@ -0,0 +1,18 @@
+using JWTAuthenticationManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).
+    AddJsonFile("ocelot.json", optional: false, reloadOnChange: true).
+    AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
+// Add services to the container.
+builder.Services.AddCustomJwtAuthentication();
+var app = builder.Build();
+await app.UseOcelot();
+// Configure the HTTP request pipeline.
+app.UseAuthentication();
+
+app.Run();
